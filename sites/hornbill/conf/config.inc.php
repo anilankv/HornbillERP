7706535248dfcc85952b00b7fe73f90a378dc314
anilankv <anilankv@gmail.com>
2025-02-17 11:12:28 +0530

    Update config.inc.php

--- sites/hornbill/conf/config.inc.php
+++ sites/hornbill/conf/config.inc.php
@@ -6,15 +6,15 @@
    $conf['dbms'] = 'postgres7';
    $conf['dbhost'] = '127.0.0.1';
    $conf['dbport'] = 5432 ;
-   $conf['dbname'] = 'ewfdbi';
-   $conf['baseurl'] = "http://ewf.atps.in/" ;
+   $conf['dbname'] = 'dbname';
+   $conf['baseurl'] = "http://hornbill.yourdomain.com/" ;
    $conf['dbuser'] = 'pubusr';
    $conf['dbpasswd'] = 'pubusr';
    $conf['dbusrgrp'] = '0';
    $conf['table_prefix'] = '';
    $conf['description'] = 'oss' ;
    $conf['timezone'] = 'Asia/Calcutta' ; 
-   $conf['upload_path'] = '/home/ewf/fileUp/';
+   $conf['upload_path'] = '/home/hornbill/fileUp/';
    $conf['MailUser'] = 'ewf@atps.in';
    $conf['DfltAddr'] = 'ewf@atps.in';
    $conf['MailPsw'] = 'vR!ZKBG6';
@@ -26,7 +26,7 @@
    $conf['servers'][0]['type'] = 'pg_oss'; // or my_oss
    $conf['servers'][0]['host'] = '127.0.0.1';  // Hostname or IP address for server.
    $conf['servers'][0]['port'] = 5432;         // Database port on server
-   $conf['servers'][0]['defaultdb'] = 'ewfdbi';  // Default database to connect to.
+   $conf['servers'][0]['defaultdb'] = 'dbname';  // Default database to connect to.
    $conf['servers'][0]['pg_dump_path'] = '/usr/bin/pg_dump';
    $conf['servers'][0]['pg_dumpall_path'] = '/usr/bin/pg_dumpall';
    $conf['extra_login_security'] = true;
