471c3354159d70cfb76c186557a98187e41b7c45
anilankv <anilankv@gmail.com>
2025-02-17 10:33:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+   define('IN_OSS', true);
+   define ( 'IN_ANONYMOUS', true ) ;
+   include_once(getcwd() . '/ol/lib/lib.inc.php');
+   include_once(getcwd() . '/ol/home.php');
+//      echo "(" . json_encode($rA) . ")" ;
+?>
